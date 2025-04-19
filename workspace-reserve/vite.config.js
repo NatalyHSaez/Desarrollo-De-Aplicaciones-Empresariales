e790dba8bab271afbc37e756f6d27bec11c776c1
NatalyHSaez <112361725+NatalyHSaez@users.noreply.github.com>
2025-04-19 14:45:21 -0400

    Update vite.config.js

--- workspace-reserve/vite.config.js
+++ workspace-reserve/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      "@": "/src",
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
