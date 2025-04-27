ba5fd7053587cd2a7e006cdc79459d8f9f164209
NatalyHSaez <mackahuaiquinao@gmail.com>
2025-04-26 23:00:34 -0400

    Update InicioAdmin.jsx

--- workspace-reserve/src/components/InicioAdmin.jsx
+++ workspace-reserve/src/components/InicioAdmin.jsx
@@ -29,6 +29,12 @@ const InicioAdmin = ({ usuario, setUsuario }) => {
           {mostrarMenuCuenta && (
             <div className="absolute right-0 mt-2 bg-white border rounded shadow-lg text-black z-10">
               <button
+                    className="block px-4 py-2 hover:bg-gray-200 w-full text-left"
+                    onClick={() => navigate("/perfil")}
+                  >
+                    Ver Perfil
+                  </button>
+              <button
                 className="block px-4 py-2 hover:bg-gray-200 w-full text-left"
                 onClick={handleLogout}
               >
